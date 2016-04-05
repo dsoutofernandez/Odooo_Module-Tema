e42a0fad0dd779cdfc80c59d791e3ff9bb659738
Danié <dsoutofernandez@danielcastelao.org>
2016-04-06 00:33:40 +0200

    Commit Tema

---
+++ theme_default_1/__openerp__.py
@@ -0,0 +1,10 @@
+{
+  'name':'Tutorial Tema Odoo',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Your name',
+
+  'data': [ 'views/layout.xml','views/pages.xml','views/assets.xml','views/options.xml'],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
